ab89561b175e1147395bac2b51296844b21eca20
sclasen <sclasen@salesforce.com>
2011-07-25 14:55:13 -0700

    fix typesafe ivy repo

--- opt/Heroku-0.10.1.scala
+++ opt/Heroku-0.10.1.scala
@@ -5,7 +5,7 @@ object Heroku extends Plugin {
   override def settings = Seq(
     externalResolvers <<= resolvers map { appResolvers =>
       Seq(Resolver.defaultLocal) ++ appResolvers ++
-      Seq(Resolver.url("heroku-sbt-typesafe") artifacts "http://maven-s3pository.herokuapp.com/typesafe-ivy/[organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext]",
+      Seq(Resolver.url("heroku-sbt-typesafe") artifacts "http://maven-s3pository.herokuapp.com/typesafe-releases-ivy/[organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext]",
           "heroku-central" at "http://maven-s3pository.herokuapp.com/central/",
           "heroku-scala-tools-releases" at "http://maven-s3pository.herokuapp.com/scala-tools-releases/",
           "heroku-scala-tools-snapshots" at "http://maven-s3pository.herokuapp.com/scala-tools-snapshots/")
