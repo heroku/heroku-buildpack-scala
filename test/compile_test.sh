2ea03ea17342b4a349d4ec595c2c4f987757cb7e
Ryan Brainard <brainard@heroku.com>
2012-01-16 14:34:42 -0800

    add support for caching ivy2 repo

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -24,40 +24,75 @@
 # build.sbt: TaskKey[Unit]("stage") in Compile := { println("Hello Staging!") }
 
 DEFAULT_SBT_VERSION="0.11.0"
+SBT_TEST_CACHE="/tmp/sbt-test-cache"
 
-setupSbt()
+createSbtProject()
 {
   sbtVersion=${1:-${DEFAULT_SBT_VERSION}}
+  projectRoot=${2:-${BUILD_DIR}}
 
-  cat > ${BUILD_DIR}/blah.scala <<EOF
+  cat > ${projectRoot}/blah.scala <<EOF
 object Hi {
   def main(args: Array[String]) = println("Hi!")
 }
 EOF
 
-  cat > ${BUILD_DIR}/build.sbt <<EOF
+  cat > ${projectRoot}/build.sbt <<EOF
 TaskKey[Unit]("stage") in Compile := { println("Hello Staging!") }
 EOF
 
-  mkdir -p ${BUILD_DIR}/project
-  cat > ${BUILD_DIR}/project/build.properties <<EOF
+  mkdir -p ${projectRoot}/project
+  cat > ${projectRoot}/project/build.properties <<EOF
 sbt.version=${sbtVersion}
 EOF
 }
 
-_testComplile()
+primeIvyCache()
 {
-  setupSbt
+  sbtVersion=${1:-${DEFAULT_SBT_VERSION}}
+
+  if [ ! -d ${SBT_TEST_CACHE}/${sbtVersion} ]; then
+    ORIGINAL_BUILD_DIR=${BUILD_DIR}
+    ORIGINAL_CACHE_DIR=${CACHE_DIR}
+
+    BUILD_DIR=${SBT_TEST_CACHE}/${sbtVersion}/app/build
+    CACHE_DIR=${SBT_TEST_CACHE}/${sbtVersion}/app/cache
+    mkdir -p ${BUILD_DIR} ${CACHE_DIR}
+
+    createSbtProject ${sbtVersion} ${BUILD_DIR}
+    ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+
+    BUILD_DIR=${ORIGINAL_BUILD_DIR}
+    CACHE_DIR=${ORIGINAL_CACHE_DIR}
+  fi
+
+  mkdir -p ${CACHE_DIR}/.sbt_home
+  cp -r ${SBT_TEST_CACHE}/${sbtVersion}/app/cache/.sbt_home/.ivy2 ${CACHE_DIR}/.sbt_home
+}
+
+testComplile()
+{
+  createSbtProject
+  primeIvyCache
+
+  mkdir -p ${CACHE_DIR}/.sbt_home/.ivy2
+  mkdir -p ${CACHE_DIR}/.sbt_home/bin
+  touch ${CACHE_DIR}/.sbt_home/.ivy2/testfile
+  touch ${CACHE_DIR}/.sbt_home/bin/testfile
 
   compile
 
   assertCapturedSuccess
   assertFileContains "Hello Staging!" "${STD_OUT}"
+  assertTrue "Ivy2 cache should have been unpacked" "[ -f ${BUILD_DIR}/.sbt_home/.ivy2/testfile ]"
+  assertTrue "SBT bin cache should have been unpacked" "[ -f ${BUILD_DIR}/.sbt_home/bin/testfile ]"
+  assertTrue "Ivy2 cache should exist" "[ -d ${BUILD_DIR}/.ivy2/cache ]"
+  assertFileContains "Building app with sbt" "${STD_OUT}"
 }
 
 testComplile_NoBuildPropertiesFile()
 {
-  setupSbt
+  createSbtProject
   rm ${BUILD_DIR}/project/build.properties
 
   compile
@@ -69,7 +104,7 @@ testComplile_NoBuildPropertiesFile()
 
 testComplile_BuildPropertiesFileWithUnsupportedVersion()
 {
-  setupSbt "0.10.0"
+  createSbtProject "0.10.0"
 
   compile
   
@@ -80,7 +115,7 @@ testComplile_BuildPropertiesFileWithUnsupportedVersion()
 
 testComplile_BuildPropertiesFileWithUnsupportedVersion()
 {
-  setupSbt "0.11.0-RC"
+  createSbtProject "0.11.0-RC"
 
   compile
   
