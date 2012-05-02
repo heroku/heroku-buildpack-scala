a0d46f0ff1a8338f033a294f16c300725191d388
Naaman Newbold <naaman@heroku.com>
2012-05-02 14:45:35 -0700

    Tests for play detect and release scripts.

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -41,6 +41,18 @@ testDetect_BuildPropertiesFileFoundInProjectDir()
   assertAppDetected "Scala"
 }
 
+testDetect_ApplicationConfFileDetectsAsPlay20()
+{
+  mkdir ${BUILD_DIR}/project
+  touch ${BUILD_DIR}/project/Build.scala
+  mkdir ${BUILD_DIR}/conf
+  touch ${BUILD_DIR}/conf/application.conf
+
+  detect
+
+  assertAppDetected "Play 2.0"
+}
+
 testDetect_NotFound()
 {
   detect  
