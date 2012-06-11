56f397ab948cd3a6f2e558b72f2cb8778d732322
Ryan Brainard <brainard@heroku.com>
2012-06-11 13:51:06 -0700

    Differentiate Play 2.0 apps between Scala and Java https://github.com/heroku/heroku-buildpack-scala/issues/13

--- test/common_test.sh
+++ test/common_test.sh
@@ -169,3 +169,26 @@ EOF
   assertCapturedSuccess
   assertCapturedEquals "${EXPECTED_VERSION}"
 }
+
+testCountFiles() {
+   mkdir -p ${BUILD_DIR}/two/three
+
+   touch ${BUILD_DIR}/1.a
+   touch ${BUILD_DIR}/1.x
+   touch ${BUILD_DIR}/two/2.a
+   touch ${BUILD_DIR}/two/2.ax
+   touch ${BUILD_DIR}/two/three/3.a
+   touch ${BUILD_DIR}/two/three/3.xa
+
+   capture count_files ${BUILD_DIR} '*.a'
+   assertCapturedSuccess
+   assertCapturedEquals "3"
+}
+
+testCountFiles_BadDir() {
+   mkdir -p ${BUILD_DIR}/something
+
+   capture count_files ${BUILD_DIR}/something_else '*.a'
+   assertCapturedSuccess
+   assertCapturedEquals "0"
+}
--- test/detect_test.sh
+++ test/detect_test.sh
@@ -41,7 +41,7 @@ testDetect_BuildPropertiesFileFoundInProjectDir()
   assertAppDetected "Scala"
 }
 
-testDetect_ApplicationConfFileDetectsAsPlay20()
+testDetect_ApplicationConfFileDetectsAsPlay20Unknown()
 {
   mkdir ${BUILD_DIR}/project
   touch ${BUILD_DIR}/project/Build.scala
@@ -50,8 +50,41 @@ testDetect_ApplicationConfFileDetectsAsPlay20()
 
   detect
 
-  assertAppDetected "Play 2.0"
+  assertAppDetected "Play 2.0 - Unknown"
 }
+
+testDetect_ApplicationConfFileDetectsAsPlay20Scala()
+{
+  mkdir ${BUILD_DIR}/project
+  touch ${BUILD_DIR}/project/Build.scala
+  mkdir ${BUILD_DIR}/conf
+  touch ${BUILD_DIR}/conf/application.conf
+  mkdir ${BUILD_DIR}/app
+  touch ${BUILD_DIR}/app/something.scala
+  mkdir ${BUILD_DIR}/app/views
+  touch ${BUILD_DIR}/app/something.scala.html
+  
+  detect
+
+  assertAppDetected "Play 2.0 - Scala"
+}
+
+testDetect_ApplicationConfFileDetectsAsPlay20Java()
+{
+  mkdir ${BUILD_DIR}/project
+  touch ${BUILD_DIR}/project/Build.scala
+  mkdir ${BUILD_DIR}/conf
+  touch ${BUILD_DIR}/conf/application.conf
+  mkdir ${BUILD_DIR}/app
+  touch ${BUILD_DIR}/app/something.java
+  mkdir ${BUILD_DIR}/app/views
+  touch ${BUILD_DIR}/app/something.scala.html
+  
+  detect
+
+  assertAppDetected "Play 2.0 - Java"
+}
+
 
 testDetect_NotFound()
 {
