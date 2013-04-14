dbbdf0fc60fae7a5e3f80aa98862bf96b07f5d10
Ryan Brainard <ryan.brainard@gmail.com>
2013-04-14 16:42:52 -0700

    skip doc generation

--- opt/Heroku-0.11.3.scala
+++ opt/Heroku-0.11.3.scala
@@ -8,5 +8,7 @@ object Heroku extends Plugin {
       Seq(Resolver.url("heroku-sbt-typesafe") artifacts "http://s3pository.heroku.com/ivy-typesafe-releases/[organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext]",
           "heroku-central" at "http://s3pository.heroku.com/maven-central/",
           "typesafe" at "http://repo.typesafe.com/typesafe/repo/")
-  })
+    },
+    sources in doc in Compile := List()
+  )
 }
