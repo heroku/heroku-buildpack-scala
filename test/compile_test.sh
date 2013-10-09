29feca0016d966e4bdb1b769b9e7eb477b0f2c8f
Ryan Brainard <brainard@heroku.com>
2013-10-09 16:33:33 -0700

    Add tests for Play trimming

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -126,6 +126,9 @@ testCompile_Play20Project() {
   assertCapturedSuccess
   assertTrue  "Ivy2 cache should have been repacked for a play project." "[ -d ${CACHE_DIR}/.sbt_home/.ivy2 ]"
   assertFalse "Ivy2 cache should not have been included in slug for a play project." "[ -d ${BUILD_DIR}/.sbt_home/.ivy2 ]"
+  assertFalse "Resolution cache should not have been included in slug for a play project." "[ -d ${BUILD_DIR}/target/resolution-cache ]"
+  assertFalse "Streams should not have been included in slug for a play project." "[ -d ${BUILD_DIR}/target/streams ]"
+  assertFalse "Scala cache should not have been included in slug for a play project." "[ -d ${BUILD_DIR}/target/scala-2.9.1 ]"
 }
 
 testCompile_WithNonDefaultVersion()
