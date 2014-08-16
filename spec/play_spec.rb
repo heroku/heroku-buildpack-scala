68cf858a6fd78ed7ea224facdc90ac5a08af519f
Joe Kutner <jpkutner@gmail.com>
2014-08-16 14:29:18 -0500

    addeding hatchet specs

---
+++ spec/play_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'spec_helper'
+
+describe "Play" do
+
+  REPOS={
+    "play-2.3.x-scala-sample" => "2.3",
+    "play-2.2.x-scala-sample" => "2.2",
+    "play-2.1.x-scala-sample" => "2.1",
+    "play-2.0.x-scala-sample" => "2.0",
+    "play-2.3.x-java-sample" => "2.3",
+    "play-2.2.x-scala-sample" => "2.2",
+    "play-2.1.x-java-sample" => "2.1",
+    "play-2.0.x-java-sample" => "2.0"
+  }
+
+  REPOS.keys.each do |repo|
+    context repo do
+      it "should not download pre-cached dependencies" do
+        Hatchet::Runner.new(repo).deploy do |app|
+          expect(app.output).to match("Running: sbt update")
+          expect(app.output).to match("Running: sbt compile stage")
+          expect(app.output).to match(/Priming Ivy cache \(Scala-2\.[0-9]{1,2}, Play-#{REPOS[repo]}\)/)
+          expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt")
+          expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/releases/com/typesafe/play/play")
+        end
+      end
+    end
+  end
+end
---
+++ spec/sbt_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'spec_helper'
+
+describe "Sbt" do
+  it "should not download pre-cached dependencies" do
+    Hatchet::Runner.new("sbt-minimal-scala-sample").deploy do |app|
+      expect(app.output).to match("Running: sbt update")
+      expect(app.output).to match("Running: sbt compile stage")
+      expect(app.output).to match(/Priming Ivy cache/)
+      expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt")
+    end
+  end
+
+  it "should run sbt-clean" do
+    app = Hatchet::Runner.new("sbt-minimal-scala-sample")
+    app.setup!
+    app.set_config("SBT_CLEAN" => "true")
+  
+    app.deploy do |app|
+      expect(app.output).to match("Running: sbt clean compile stage")
+    end
+  end
+end
