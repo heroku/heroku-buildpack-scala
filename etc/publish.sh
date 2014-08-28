b0b4a6c13da65946983fd32d32fdc8918c425ecf
Joe Kutner <jpkutner@gmail.com>
2014-08-28 07:54:34 -0500

    added heroku plugin back, and a test

--- etc/publish.sh
+++ etc/publish.sh
@@ -6,7 +6,7 @@ if [ ! -z "$1" ]; then
   git clone git@github.com:heroku/heroku-buildpack-scala.git &&
   cd heroku-buildpack-scala &&
   git checkout master &&
-  find . -not -name 'bin' -maxdepth 1 -delete &&
+  find . ! -name 'bin' ! -name 'opt' -maxdepth 1 -delete &&
   heroku buildpacks:publish $1/scala
   popd > /dev/null 2>&1
   echo "Cleaning up..."
--- spec/sbt_spec.rb
+++ spec/sbt_spec.rb
@@ -3,10 +3,11 @@ require_relative 'spec_helper'
 describe "Sbt" do
   it "should not download pre-cached dependencies" do
     Hatchet::Runner.new("sbt-minimal-scala-sample").deploy do |app|
-      expect(app.output).to match("Running: sbt update")
-      expect(app.output).to match("Running: sbt compile stage")
-      expect(app.output).to match(/Priming Ivy cache/)
-      expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt")
+      expect(app.output).to include("Running: sbt update")
+      expect(app.output).to include("Running: sbt compile stage")
+      expect(app.output).to include("Priming Ivy cache")
+      expect(app.output).not_to include("downloading http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt")
+      expect(app.output).not_to include("Main Scala API documentation to")
     end
   end
 
