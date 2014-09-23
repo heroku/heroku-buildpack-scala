20db75745fc4fa7b019d2e1716d141211aef6e4e
Joe Kutner <jpkutner@gmail.com>
2014-09-23 09:41:23 -0500

    revised publish script [skip ci]

--- etc/publish.sh
+++ etc/publish.sh
@@ -6,7 +6,7 @@ if [ ! -z "$1" ]; then
   git clone git@github.com:heroku/heroku-buildpack-scala.git &&
   cd heroku-buildpack-scala &&
   git checkout master &&
-  find . ! -name 'bin' ! -name 'opt' -maxdepth 1 -delete &&
+  find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf -- &&
   heroku buildpacks:publish $1/scala
   popd > /dev/null 2>&1
   echo "Cleaning up..."
