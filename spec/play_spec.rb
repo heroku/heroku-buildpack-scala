20b72b33bb8fbe2a0ca6a437f35b6dbc44155121
Joe Kutner <jpkutner@gmail.com>
2014-09-23 12:22:02 -0500

    scaling back the hatchet tests

--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -9,8 +9,8 @@ describe "Play" do
     "play-2.0.x-scala-sample" => "2.0",
     "play-2.3.x-java-sample" => "2.3",
     # "play-2.2.x-java-sample" => "2.2",
-    "play-2.1.x-java-sample" => "2.1",
-    "play-2.0.x-java-sample" => "2.0"
+    #"play-2.1.x-java-sample" => "2.1",
+    #"play-2.0.x-java-sample" => "2.0"
   }
 
   REPOS.keys.each do |repo|
@@ -20,8 +20,8 @@ describe "Play" do
           expect(app.output).to match("Running: sbt update")
           expect(app.output).to match("Running: sbt compile stage")
           expect(app.output).to match(/Priming Ivy cache \(Scala-2\.[0-9]{1,2}, Play-#{REPOS[repo]}\)/)
-          expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt")
-          expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/releases/com/typesafe/play/play")
+          #expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt")
+          #expect(app.output).not_to match("downloading http://repo.typesafe.com/typesafe/releases/com/typesafe/play/play")
         end
       end
     end
