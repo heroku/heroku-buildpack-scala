c17e3a7f48df92e904f384f8e070fde4949bdc29
Joe Kutner <jpkutner@gmail.com>
2015-01-04 10:10:07 -0600

    Updated publishing script to tag the right directory, as long as the commit has is correct [skip ci]

--- etc/publish.sh
+++ etc/publish.sh
@@ -10,19 +10,27 @@ if [ ! -z "$1" ]; then
   git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
   cd heroku-buildpack-$BP_NAME
   git checkout master
+  headHash=$(git rev-parse HEAD)
+
   find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf --
   heroku buildpacks:publish $1/$BP_NAME
 
   if [ "$1" = "heroku" ]; then
-    echo "Tagging commit... "
     newTag=$(heroku buildpacks:revisions heroku/$BP_NAME | sed -n 2p | grep -o -e "v\d*")
-    git tag $newTag
-    git push --tags
   fi
 
   popd > /dev/null 2>&1
   echo "Cleaning up..."
   rm -rf /tmp/heroku-buildpack-$BP_NAME
+
+  if [ "$1" = "heroku" ]; then
+    if [ "$headHash" = "$(git rev-parse HEAD)" ]; then
+      echo "Tagging commit $headHash with $newTag... "
+      git tag $newTag
+      git push --tags
+    fi
+  fi
+
   echo "Done."
 else
   echo "You must provide a buildkit organization as an argument!"
