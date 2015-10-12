3fa91c8a7caa1ba43042c30a109d5e709bc8a19d
Joe Kutner <jpkutner@gmail.com>
2015-10-12 08:23:24 -0500

    Added failure message for 'is already defined as object'

--- lib/failures.sh
+++ lib/failures.sh
@@ -5,6 +5,12 @@ handle_sbt_errors() {
 
   local header="Failed to run sbt!"
 
+  local previousVersion="You can also try reverting to the previous version of the buildpack by running:
+$ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-scala#previous-version"
+
+  local footer="Thanks,
+Heroku"
+
   if grep -qi 'Not a valid key: stage' "$log_file"; then
     error "${header}
 It looks like your build.sbt does not have a valid 'stage' task.
@@ -13,16 +19,31 @@ https://devcenter.heroku.com/articles/scala-support#build-behavior
 If you continue to have problems, please submit a ticket so we can help:
 http://help.heroku.com
 
-Thanks,
-Heroku"
+${footer}"
+  elif grep -qi 'is already defined as object' "$log_file"; then
+    error "${header}
+We're sorry this build is failing. It looks like you may need to run a
+clean build to remove any stale SBT caches. You can do this by setting
+a configuration variable like this:
+
+    $ heroku config:set SBT_CLEAN=true
+
+Then deploy you application with 'git push' again. If the build succeeds
+you can remove the variable by running this command
+
+    $ heroku config:unset SBT_CLEAN
+
+If this does not resolve the problem, please submit a ticket so we
+can help: https://help.heroku.com
+${previousVersion}
+
+${footer}"
   else
     error "${header}
-We're sorry this build is failing! If you can't find the issue in application
+We're sorry this build is failing. If you can't find the issue in application
 code, please submit a ticket so we can help: https://help.heroku.com
-You can also try reverting to the previous version of the buildpack by running:
-$ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-scala#previous-version
+${previousVersion}
 
-Thanks,
-Heroku"
+${footer}"
   fi
 }
