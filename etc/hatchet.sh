4f968d975ec3a318a1401879c3b8d8195fdc7ffd
Joe Kutner <jpkutner@gmail.com>
2017-08-01 08:15:37 -0500

    Added Circle CI config

--- etc/hatchet.sh
+++ etc/hatchet.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+BUILDPACK_NAME="heroku-buildpack-scala"
+
+if [ "$CIRCLECI" == "true" && -n "$CI_PULL_REQUEST" ]; then
+  if [ "$CIRCLE_PR_USERNAME" != "heroku" ]; then
+    echo "Skipping integration tests on forked PR."
+    exit 0
+  fi
+fi
+
+if [ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  if [ "$TRAVIS_PULL_REQUEST_SLUG" != "heroku/$BUILDPACK_NAME" ]; then
+    echo "Skipping integration tests on forked PR."
+    exit 0
+  fi
+fi
+
 if [ -z "$HEROKU_API_KEY" ]; then
-  echo "Missing \$HEROKU_API_KEY. Skipping."
-  exit 0
-elif [ -n "$CIRCLE_BRANCH" ]; then
+  echo "ERROR: Missing \$HEROKU_API_KEY."
+  exit 1
+fi
+
+if [ -n "$CIRCLE_BRANCH" ]; then
   export HATCHET_BUILDPACK_BRANCH="$CIRCLE_BRANCH"
 elif [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
   export HATCHET_BUILDPACK_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
@@ -17,5 +35,5 @@ bundle install
 bundle exec hatchet install &&
 HATCHET_RETRIES=3 \
 HATCHET_DEPLOY_STRATEGY=git \
-HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-scala.git" \
+HATCHET_BUILDPACK_BASE="https://github.com/heroku/$BUILDPACK_NAME.git" \
 bundle exec rspec spec/
