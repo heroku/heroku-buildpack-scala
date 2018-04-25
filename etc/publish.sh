3de65fc162811c4e45b98fa77ad7f2f310e29643
Joe Kutner <jpkutner@gmail.com>
2018-04-25 15:15:50 -0500

    Added new publish script and buildpack.toml

--- etc/publish.sh
+++ etc/publish.sh
@@ -2,47 +2,33 @@
 
 set -e
 
-BP_NAME="scala"
+BP_NAME=${1:-"heroku/scala"}
 
-if [ ! -z "$1" ]; then
-  pushd . > /dev/null 2>&1
-  cd /tmp
-  rm -rf heroku-buildpack-$BP_NAME
-  git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
-  cd heroku-buildpack-$BP_NAME
-  git checkout master
-  headHash=$(git rev-parse HEAD)
+curVersion=$(heroku buildpacks:versions $BP_NAME | awk 'FNR == 3 { print $1 }')
+newVersion="v$((curVersion + 1))"
 
-  find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' \
-         ! -name 'lib' -maxdepth 1 -print0 | xargs -0 rm -rf --
-  heroku buildkits:publish $1/$BP_NAME
+read -p "Deploy as version: $newVersion [y/n]? " choice
+case "$choice" in
+  y|Y ) echo "";;
+  n|N ) exit 0;;
+  * ) exit 1;;
+esac
 
-  if [ "$1" = "heroku" ]; then
-    newTag=$(heroku buildkits:revisions heroku/$BP_NAME | sed -n 2p | grep -o -e "v\d*")
-  fi
+originMaster=$(git rev-parse origin/master)
+echo "Tagging commit $originMaster with $newVersion... "
+git tag $newVersion ${originMaster:?}
+git push origin refs/tags/$newVersion
 
-  popd > /dev/null 2>&1
-  echo "Cleaning up..."
-  rm -rf /tmp/heroku-buildpack-$BP_NAME
+heroku buildpacks:publish $BP_NAME $newVersion
 
-  if [ "$1" = "heroku" ]; then
-    if [ "$headHash" = "$(git rev-parse HEAD)" ]; then
-      echo "Tagging commit $headHash with $newTag... "
-      git tag $newTag
-      echo "Updating previous-version tag"
-      git tag -d previous-version
-      git push origin :previous-version
-      git tag previous-version latest-version
-      echo "Updating latest-version tag"
-      git tag -d latest-version
-      git push origin :latest-version
-      git tag latest-version
-      git push --tags
-    fi
-  fi
+echo "Updating previous-version tag"
+git tag -d previous-version
+git push origin :previous-version
+git tag previous-version latest-version
+echo "Updating latest-version tag"
+git tag -d latest-version
+git push origin :latest-version
+git tag latest-version ${originMaster:?}
+git push --tags
 
-  echo "Done."
-else
-  echo "You must provide a buildkit organization as an argument!"
-  exit 1
-fi
+echo "Done."
