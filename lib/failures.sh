64ccc8ebce2c5dd8514732351cc01fab8ecb744d
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-09-30 14:09:15 +0200

    Extend Makefile and apply formatting (#264)

--- lib/failures.sh
+++ lib/failures.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 handle_sbt_errors() {
-  local log_file="$1"
+	local log_file="$1"
 
-  local header="Failed to run sbt!"
+	local header="Failed to run sbt!"
 
-  local previousVersion="You can also try reverting to the previous version of the buildpack by running:
+	local previousVersion="You can also try reverting to the previous version of the buildpack by running:
 $ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-scala#previous-version"
 
-  local footer="Thanks,
+	local footer="Thanks,
 Heroku"
 
-  if grep -qi 'Not a valid key: stage' "$log_file"; then
-    error "${header}
+	if grep -qi 'Not a valid key: stage' "$log_file"; then
+		error "${header}
 It looks like your build.sbt does not have a valid 'stage' task.
 Please read our Dev Center article for information on how to create one:
 https://devcenter.heroku.com/articles/scala-support#build-behavior
@@ -20,8 +20,8 @@ If you continue to have problems, please submit a ticket so we can help:
 http://help.heroku.com
 
 ${footer}"
-  elif grep -qi 'is already defined as object' "$log_file"; then
-    error "${header}
+	elif grep -qi 'is already defined as object' "$log_file"; then
+		error "${header}
 We're sorry this build is failing. It looks like you may need to run a
 clean build to remove any stale SBT caches. You can do this by setting
 a configuration variable like this:
@@ -38,12 +38,12 @@ can help: https://help.heroku.com
 ${previousVersion}
 
 ${footer}"
-  else
-    error "${header}
+	else
+		error "${header}
 We're sorry this build is failing. If you can't find the issue in application
 code, please submit a ticket so we can help: https://help.heroku.com
 ${previousVersion}
 
 ${footer}"
-  fi
+	fi
 }
--- lib/properties.sh
+++ lib/properties.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 get_property() {
-  local propFile=$1
-  local propName=$2
-  local propDefault=${3:-""}
+	local propFile=$1
+	local propName=$2
+	local propDefault=${3:-""}
 
-  if [ -f "${propFile}" ]; then
-    local propValue
-    propValue=$(sed '/^\#/d' "${propFile}" | grep "${propName}"  | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-    echo "${propValue:-$propDefault}"
-  else
-    echo "${propDefault}"
-  fi
+	if [ -f "${propFile}" ]; then
+		local propValue
+		propValue=$(sed '/^\#/d' "${propFile}" | grep "${propName}" | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+		echo "${propValue:-$propDefault}"
+	else
+		echo "${propDefault}"
+	fi
 }
