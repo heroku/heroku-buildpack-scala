248a94e78cbccf22bf0acb2550aa2b4a702814c1
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-10-28 14:18:40 +0100

    Add cache integration testing (#284)

---
+++ test/spec/fixtures/repos/sbt-0.11.7-play-3.x-scala-2.13.x/app/controllers/HomeController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+}
