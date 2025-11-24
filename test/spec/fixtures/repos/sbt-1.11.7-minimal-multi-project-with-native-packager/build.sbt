6e2d521e9835438330d376aee0aff2f487cd708c
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-11-24 16:01:04 +0100

    Rename test fixture to minimal-multi-project-with-native-packager

--- test/spec/fixtures/repos/sbt-1.11.7-minimal-multi-project-with-native-packager/build.sbt
+++ test/spec/fixtures/repos/sbt-1.11.7-minimal-multi-project-with-native-packager/build.sbt
@@ -1,13 +1,13 @@
 lazy val root = (project in file("."))
   .aggregate(subproject)
   .settings(
-    name := "sbt-1.11.7-multi-project-root"
+    name := "sbt-1.11.7-minimal-multi-project-with-native-packager-root"
   )
 
 lazy val subproject = (project in file("subproject"))
   .enablePlugins(JavaAppPackaging)
   .settings(
-    name := "subproject",
+    name := "sbt-1.11.7-minimal-multi-project-with-native-packager-subproject",
     scalaVersion := "2.13.17",
     libraryDependencies ++= Seq(
       "dev.zio" %% "zio-http" % "3.5.1"
--- test/spec/sbt_multi_project_spec.rb
+++ test/spec/sbt_multi_project_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 
 describe 'Sbt multi-project builds' do
   it 'applies project prefix to all tasks when SBT_PROJECT is set' do
-    new_default_hatchet_runner('sbt-1.11.7-multi-project-with-native-packager').tap do |app|
+    new_default_hatchet_runner('sbt-1.11.7-minimal-multi-project-with-native-packager').tap do |app|
       app.before_deploy do
         app.set_config('SBT_PROJECT' => 'subproject')
       end
@@ -49,7 +49,7 @@ describe 'Sbt multi-project builds' do
   end
 
   it 'applies project prefix to custom tasks when SBT_PROJECT and SBT_TASKS are both set' do
-    new_default_hatchet_runner('sbt-1.11.7-multi-project-with-native-packager').tap do |app|
+    new_default_hatchet_runner('sbt-1.11.7-minimal-multi-project-with-native-packager').tap do |app|
       app.before_deploy do
         app.set_config('SBT_PROJECT' => 'subproject')
         app.set_config('SBT_TASKS' => 'clean compile stage')
